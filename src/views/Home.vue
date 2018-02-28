5b1aea30af6a8817217034f0434811697438989e
Brandon Lyons <lyonsbp@umich.edu>
2018-02-27 22:22:20 -0500

    added margins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <v-layout align-center>
         <v-flex xs12>
           <v-jumbotron dark height="200px" color="grey darken-4" class="elevation-5" ripple>
-            <h3 class="display-3">Hello, friend</h3>
+            <h3 class="display-3 mt-4">Hello, friend</h3>
             <span class="subheading">My name is Brandon Lyons
               <br/>
               Learning new technologies to build a cleaner user experience is my idea of fun
