ff2000d78914c8a50823eae504093044a64e5976
Brandon Lyons <lyonsbp@umich.edu>
2018-03-03 05:37:26 -0500

    added job information

--- src/components/ExperienceComponent.vue
+++ src/components/ExperienceComponent.vue
@@ -1,15 +1,30 @@
 <template>
-    <v-card dark color="grey darken-4" class="elevation-10" ripple>
+    <v-card dark color="grey darken-4" class="elevation-10">
         <v-card-title primary-title>
             <div class="headline ml-2">Work History</div>
         </v-card-title>
         <v-divider class="mb-2"></v-divider>
-            <v-expansion-panel popout>
-                <v-expansion-panel-content v-for="(item, index) in jobs" :key="index" >
+            <v-expansion-panel inset>
+                <v-expansion-panel-content ripple v-for="(item, index) in jobs" :key="index" class="mb-1 mt-1">
                     <div slot="header">
-                        <div class="headline">{{item.title}}</div>
-                        <p>{{item.summary}}</p>
+                        <div class="headline">{{item.title}} - {{item.employer}}</div>
+                        <p class="ml-2">{{item.summary}}</p>
                     </div>
+                    <v-divider v-if="item.accomplishments.length"></v-divider>
+                    <v-card>
+                        <v-card-title>
+                            <div class="title">Accomplishments</div>
+                        </v-card-title>
+                        <v-card-text>
+                            <div v-if="item.accomplishments.length" class="ml-4">
+                                <template v-for="(text, index) in item.accomplishments">
+                                    <div class="subheading mb-2" :key="index">
+                                        <v-icon class="mx-2" :key="index">memory</v-icon>{{text}}
+                                    </div>
+                                </template>
+                            </div>
+                        </v-card-text>
+                    </v-card>
                 </v-expansion-panel-content>
             </v-expansion-panel>
     </v-card>
@@ -27,19 +42,23 @@ export default class ExperienceComponent extends Vue {
                     title: 'Research Assistant',
                     employer: 'Undergradute Research Opportunities at University of Michigan',
                     summary: 'Helped to test software by identifying bugs, collaborated on large sets of data with peers.',
-
+                    accomplishments: ['Help to identify and debug issues within a CUDA parallel c++ module.', 'Gained experience effectively collaborating with a team.']
                 },
                 {
                     title: 'Desktop Support Temp',
                     employer: 'University of Michigan',
-                    summary: 'Worked with a close-knit team to solve users’ issues with both Mac and Windows based systems within ServiceNow, a ticket-based reporting system.',
-
+                    summary: 'Worked to deliver simple solutions to complex problems to our customers throughout the university using ServiceNow, a ticket-based reporting system.',
+                    accomplishments: ['Utilized BlueCat network manager to control network secturiy across different campus networks.',
+                                      'Built and configured Linux, Mac, and PC systems with automated deployment tools.'
+                                     ]
                 },
                 {
                     title: 'Web Development Intern',
                     employer: 'University of Michigan Health System',
                     summary: 'Played a major role in the architecture, design, and implementation of a modern single-page application using Typescript, C# .NET, and PL/SQL.',
-
+                    accomplishments: ['Converted depreciating Coldfusion web application to a modern, responsive webapp using Vue.js, Typescript, jQuery and ASMX webservices.',
+                                        'Developed webapp to serve as an internal customer interface for the Oracle database at the UMich Health Systems Data Warehouse.',
+                                     ]
                 }
             ]
         };
